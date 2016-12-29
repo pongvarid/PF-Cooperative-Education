dc0f5d491c1c0e4d703f22dc50606d2b741bc5f8
Fluke\FlukePC <flukebevil@gmail.com>
2016-12-29 21:31:40 +0700

    final table sort and search

--- conpany_stu.php
+++ conpany_stu.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 
+
+
+
  <?php include './style.php';
 $style = new style();
 $style->head("");
@@ -12,11 +15,64 @@ include './sqlcon.php';
 include './navbar.php';
 $nav = new navbar();
 
-$sql = "SELECT * FROM company";
+
+
+$sort = 'ASC';
+
+
+
+
+if (isset($_GET["sort"])||isset($_GET["sort2"])) {
+	
+	//min to max
+	if($_GET["sort"] == 'ASC'){
+		$sort = 'DESC';
+		
+		if ($_GET["sort2"]=='name') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN ASC";
+		}elseif ($_GET["sort2"]=='add') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN ASC";
+		}elseif ($_GET["sort2"]=='posi') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN ASC";
+		}elseif ($_GET["sort2"]=='maj') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN ASC";
+		}elseif ($_GET["sort2"]=='status') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN ASC";
+		}
+
+		//max to min
+	}elseif ($_GET["sort"]=='DESC') {
+		$sort='ASC';
+		if ($_GET["sort2"]=='name') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN DESC";
+		}elseif ($_GET["sort2"]=='add') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN DESC";
+		}elseif ($_GET["sort2"]=='posi') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN DESC";
+		}elseif ($_GET["sort2"]=='maj') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN DESC";
+		}elseif ($_GET["sort2"]=='status') {
+			$sql = "SELECT * FROM company ORDER BY NameCPN DESC";
+		}
+	}
+}else{ $sql = "SELECT * FROM company"; }
+
+
+
+
+
+
+
+
+
+
+
+
 mysqli_set_charset($con,"set character set utf8");
 $result = mysqli_query($con,$sql);
 
 $op ='';
+
 ?>
 </head>
 <body>
@@ -32,22 +88,26 @@ $op ='';
 		<br>
 		<div class="form-group">
 			<div class="input-group">
-			<input type="text" name="search_text" id="search_text" placeholder="ค้นหาจากข้อมูล" class="form-control" />	<span class="input-group-addon glyphicon glyphicon-search"></span>
+
+			<input type="text" placeholder="ค้นหาจากข้อมูล" class="form-control" id="myInput" onkeyup="myFunction()" />	<span class="input-group-addon glyphicon glyphicon-search"></span>
+
+
+			<!-- <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name"> -->
                               
 			</div>
 		</div>
 		<br>
 		
-		<div class="table-responsive">
-				<table class="table table bordered">
+		<div class="table-responsive ">
+				<table class="table table bordered " id="myTable" >
 				<thead>
-					<tr>
-						<th>ชื่อ</th>
-						<th>ที่อยู่</th>
-						<th>ตำแหน่งที่รองรับ</th>
-						<th>สาขาที่รองรับ</th>
-						<th>สถานะ</th>
-						<th>รายละเอียด</th>
+					<tr class="danger">
+						<th> <a href="conpany_stu.php?sort=<?php echo $sort; ?>&sort2=name"> ชื่อ </a></th>
+						<th><a href="conpany_stu.php?sort=<?php echo $sort; ?>&sort2=add"> ที่อยู่ </a></th>
+						<th><a href="conpany_stu.php?sort=<?php echo $sort; ?>&sort2=posi"> ตำแหน่งที่รองรับ </a></th>
+						<th><a href="conpany_stu.php?sort=<?php echo $sort; ?>&sort2=maj"> สาขาที่รองรับ </a></th>
+						<th><a href="conpany_stu.php?sort=<?php echo $sort; ?>&sort2=status"> สถานะ </a></th>
+						<th> รายละเอียด </th>
 					</tr>
 				</thead>
 				<?php 
@@ -84,11 +144,47 @@ $op ='';
 		
 		</div>
 	</div>
+	<script>
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display="ไม่พบข้อมูล";
+      }
+    }       
+  }
+}
+</script>
+
 </body>
 </html>
 
 
 
-
-
+<script>
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 </script>
